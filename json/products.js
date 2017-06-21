e74065cd74ea49f74299c8103d2f3d6c72b0e7cc
梁龙 <2068860045@qq.com>
2017-06-21 23:30:51 +0800

    first commit

---
+++ json/products.js
@@ -0,0 +1,428 @@
+function getJson(){
+	return {
+		erding:[{
+			number : 'ED1',
+			numberName : '耳钉',
+			banner : ['../images/erding/ED1/banner.png'],
+			showList : ['../images/erding/ED1/show1.png','../images/erding/ED1/show2.png','../images/erding/ED1/show3.png','../images/erding/ED1/show4.png','../images/erding/ED1/show5.png','../images/erding/ED1/show6.png'],
+			big : ['../images/erding/ED1/bannerBig.png']
+		},{
+			number : 'ED2',
+			numberName : '耳钉',
+			banner : ['../images/erding/ED2/banner.png'],
+			big : ['../images/erding/ED2/bannerBig.png']
+		},{
+			number : 'ED3',
+			numberName : '耳钉',
+			banner : ['../images/erding/ED3/banner.png'],
+			big : ['../images/erding/ED3/bannerBig.png']
+		},{
+			number : 'ED4',
+			numberName : '耳钉',
+			banner : ['../images/erding/ED4/banner.png'],
+			showList : ['../images/erding/ED4/show1.png','../images/erding/ED4/show2.png'],
+			big : ['../images/erding/ED4/bannerBig.png']
+		}],
+		xianglian:[{
+			number : 'AD1',
+			numberName : '项链',
+			banner : ['../images/xianglian/AD1/banner1.png','../images/xianglian/AD1/banner2.png'],
+			showList : ['../images/xianglian/AD1/show1.png','../images/xianglian/AD1/show2.png','../images/xianglian/AD1/show3.png'],
+			big : ['../images/xianglian/AD1/bannerBig1.png','../images/xianglian/AD1/bannerBig2.png']
+		},{
+			number : 'AD2',
+			numberName : '项链',
+			banner : ['../images/xianglian/AD2/banner1.png','../images/xianglian/AD2/banner2.png'],
+			big : ['../images/xianglian/AD2/bannerBig1.png','../images/xianglian/AD2/bannerBig2.png']
+		},{
+			number : 'AD3',
+			numberName : '项链',
+			banner : ['../images/xianglian/AD3/banner1.png','../images/xianglian/AD3/banner2.png','../images/xianglian/AD3/banner3.png'],
+			showList : ['../images/xianglian/AD3/show1.png','../images/xianglian/AD3/show2.png','../images/xianglian/AD3/show3.png','../images/xianglian/AD3/show4.png'],
+			big : ['../images/xianglian/AD3/bannerBig1.png','../images/xianglian/AD3/bannerBig2.png','../images/xianglian/AD3/bannerBig3.png']
+		},{
+			number : 'AD4',
+			numberName : '项链',
+			banner : ['../images/xianglian/AD4/banner.png'],
+			big : ['../images/xianglian/AD4/bannerBig.png']
+		},{
+			number : 'AD5',
+			numberName : '项链',
+			banner : ['../images/xianglian/AD5/banner.png'],
+			big : ['../images/xianglian/AD5/bannerBig.png']
+		},{
+			number : 'AD6',
+			numberName : '项链',
+			banner : ['../images/xianglian/AD6/banner.png'],
+			big : ['../images/xianglian/AD6/bannerBig.png']
+		},{
+			number : 'AD7',
+			numberName : '项链',
+			banner : ['../images/xianglian/AD7/banner.png'],
+			big : ['../images/xianglian/AD7/bannerBig.png']
+		},{
+			number : 'AD8',
+			numberName : '项链',
+			banner : ['../images/xianglian/AD8/banner.png'],
+			big : ['../images/xianglian/AD8/bannerBig.png']
+		},{
+			number : 'AD9',
+			numberName : '项链',
+			banner : ['../images/xianglian/AD9/banner.png'],
+			big : ['../images/xianglian/AD9/bannerBig.png']
+		}],
+		luoshi:[{
+			number : 'LS',
+			numberName : '裸石',
+			banner : ['../images/luoshi/banner.png'],
+			big : ['../images/luoshi/bannerBig.png']
+		}],
+		shoulian:[{
+			number : 'SL',
+			numberName : '裸石',
+			banner : ['../images/shoulian/banner.png'],
+			showList : ['../images/shoulian/show1.png','../images/shoulian/show2.png','../images/shoulian/show3.png','../images/shoulian/show4.png','../images/shoulian/show5.png'],
+			big : ['../images/shoulian/bannerBig.png']
+		}],
+		ring:[{
+			name : '哥伦比亚白金钻石群镶祖母绿订婚经典款',
+			number : 'A1',
+			numberName : '戒指',
+			price : 39999,
+			size : {
+				main : '哥伦比亚祖母绿 2.88ct',
+				color : 'Green',
+				clear : 'SI1 级',
+				cut : '枕形',
+				match : '99颗钻石 18k白金'
+			},
+			banner : ['../images/ring/A1/banner.png'],
+			showList : ['../images/ring/A1/show1.png','../images/ring/A1/show2.png','../images/ring/A1/show3.png','../images/ring/A1/show4.png'],
+			des : ['经典光芒，熠熠生辉','精工缔造，华丽镶嵌'],
+			big : ['../images/ring/A1/bannerBig.png']
+		},{
+			name : '哥伦比亚简约经典祖母绿切金戒',
+			number : 'A2',
+			numberName : '戒指',
+			price : 31999,
+			size : {
+				main : '哥伦比亚祖母绿1.68ct',
+				color : 'Vivid green',
+				clear : 'SI2',
+				cut : '祖母绿切割',
+				match : '99颗钻石 18k白金'
+			},
+			banner : ['../images/ring/A2/banner1.png','../images/ring/A2/banner2.png'],
+			showList : ['../images/ring/A2/show1.png','../images/ring/A2/show2.png','../images/ring/A2/show3.png'],
+			des : ['匠心之作','璀璨由内而外'],
+			big : ['../images/ring/A2/bannerBig1.png','../images/ring/A2/bannerBig2.png']
+		},{
+			name : '哥伦比亚黄金水滴祖母绿钻石群镶婚戒',
+			number : 'A3',
+			numberName : '戒指',
+			price : 43999,
+			size : {
+				main : '2.1ct',
+				color : 'Vivid green',
+				clear : 'VS',
+				cut : '水滴状切割',
+				match : '88颗钻石群镶 18k黄金'
+			},
+			banner : ['../images/ring/A3/banner1.png','../images/ring/A3/banner2.png'],
+			showList : ['../images/ring/A3/show1.png','../images/ring/A3/show2.png','../images/ring/A3/show3.png','../images/ring/A3/show4.png','../images/ring/A3/show5.png','../images/ring/A3/show3.png'],
+			des : ['臻美品质，璀璨绽放','时尚典雅，镂空工艺'],
+			big : ['../images/ring/A3/bannerBig1.png','../images/ring/A3/bannerBig2.png']
+		},{
+			name : '哥伦比亚白金枕形祖母绿钻石围镶戒',
+			number : 'A4',
+			numberName : '戒指',
+			price : 69999,
+			size : {
+				main : '2.9 ct',
+				color : 'Vivid green',
+				clear : 'VS（无暇）',
+				cut : '枕形切割',
+				match : '99颗钻石满镶 18k白金'
+			},
+			banner : ['../images/ring/A4/banner1.png','../images/ring/A4/banner2.png'],
+			showList : ['../images/ring/A4/show1.png','../images/ring/A4/show2.png','../images/ring/A4/show3.png','../images/ring/A4/show4.png','../images/ring/A4/show5.png','../images/ring/A4/show6.png'],
+			des : ['跃动的光彩','炫若星辰，耀目绽放'],
+			big : ['../images/ring/A4/bannerBig1.png','../images/ring/A4/bannerBig2.png']
+		},{
+			name : '哥伦比亚黄金水滴形祖母绿',
+			number : 'A5',
+			numberName : '戒指',
+			price : 19999,
+			size : {
+				main : '1.4 ct',
+				color : 'Green intensivo',
+				clear : 'SI 2',
+				cut : '水滴形切割',
+				match : '18k 黄金'
+			},
+			banner : ['../images/ring/A5/banner1.png','../images/ring/A5/banner2.png'],
+			showList : ['../images/ring/A5/show1.png','../images/ring/A5/show2.png','../images/ring/A5/show3.png'],
+			des : ['手工镶嵌，公整牢固','舒适光滑内壁'],
+			big : ['../images/ring/A5/bannerBig1.png','../images/ring/A5/bannerBig2.png']
+		},{
+			name : '钻石群镶祖母绿黄金戒',
+			number : 'A6',
+			numberName : '戒指',
+			price : 59999,
+			size : {
+				main : '祖母绿 2.5ct',
+				color : 'green intensive',
+				clear : 'Si2',
+				cut : '全镶8颗30分钻石 半嵌小钻 18k黄金',
+				match : '18k 黄金'
+			},
+			banner : ['../images/ring/A6/banner1.png','../images/ring/A6/banner2.png'],
+			showList : ['../images/ring/A6/show1.jpg','../images/ring/A6/show2.jpg','../images/ring/A6/show3.jpg'],
+			des : ['美轮美奂， 有口皆碑','立体群镶，闪耀绚烂'],
+			big : ['../images/ring/A6/bannerBig1.png','../images/ring/A6/bannerBig2.png']
+		},{
+			banner : ['../images/ring/A7/banner.png'],
+			number : 'A7',
+			numberName : '戒指',
+			big : ['../images/ring/A7/bannerBig.png']
+		},{
+			banner : ['../images/ring/A8/banner.png'],
+			number : 'A8',
+			numberName : '戒指',
+			big : ['../images/ring/A8/bannerBig.png']
+		},{
+			banner : ['../images/ring/A9/banner.png'],
+			number : 'A9',
+			numberName : '戒指',
+			big : ['../images/ring/A9/bannerBig.png']
+		},{
+			banner : ['../images/ring/A10/banner.png'],
+			number : 'A10',
+			numberName : '戒指',
+			big : ['../images/ring/A10/bannerBig.png']
+		},{
+			banner : ['../images/ring/A11/banner.png'],
+			number : 'A11',
+			numberName : '戒指',
+			big : ['../images/ring/A11/bannerBig.png']
+		},{
+			banner : ['../images/ring/A12/banner.png'],
+			number : 'A12',
+			numberName : '戒指',
+			big : ['../images/ring/A12/bannerBig.png']
+		},{
+			banner : ['../images/ring/A13/banner.png'],
+			showList : ['../images/ring/A13/show1.jpg','../images/ring/A13/show2.jpg'],
+			number : 'A13',
+			numberName : '戒指',
+			big : ['../images/ring/A13/bannerBig.png']
+		}],
+		xingzuo:[{
+			banner : ['../images/xingzuo/by/banner.png'],
+			showList : ['../images/xingzuo/by/show1.png','../images/xingzuo/by/show2.png','../images/xingzuo/by/show3.png','../images/xingzuo/by/show4.png','../images/xingzuo/by/show5.png'],
+			big : ['../images/xingzuo/by/bannerBig.png'],
+			name : '星座锁骨链-白羊座',
+			number : 'Aries',
+			numberName : '白羊座',
+			price : 6999,
+			size : {
+				main : '',
+				color : 'vividgreen',
+				clear : 'vs',
+				cut : '10-20分大小相近的祖母绿',
+				match : ''
+			},
+			des : ["If you work hard, you won't regret it",""]
+		},{
+			banner : ['../images/xingzuo/cn/banner.png'],
+			showList : ['../images/xingzuo/cn/show1.png','../images/xingzuo/cn/show2.png','../images/xingzuo/cn/show3.png','../images/xingzuo/cn/show4.png'],
+			big : ['../images/xingzuo/cn/bannerBig.png'],
+			name : '星座锁骨链-处女座',
+			number : 'Virgo',
+			numberName : '处女座',
+			price : 7799,
+			size : {
+				main : '',
+				color : 'vividgreen',
+				clear : 'vs',
+				cut : '10-20分大小相近的祖母绿',
+				match : ''
+			},
+			des : ["No matter what, we should attain the perfection in our mind.",""]
+		},{
+			banner : ['../images/xingzuo/jn/banner.png'],
+			showList : ['../images/xingzuo/jn/show1.png','../images/xingzuo/jn/show2.png','../images/xingzuo/jn/show3.png','../images/xingzuo/jn/show4.png'],
+			big : ['../images/xingzuo/jn/bannerBig.png'],
+			name : '星座锁骨链-金牛座',
+			number : 'Taurus',
+			numberName : '金牛座',
+			price : 7799,
+			size : {
+				main : '',
+				color : 'vividgreen',
+				clear : 'vs',
+				cut : '10-20分大小相近的祖母绿',
+				match : ''
+			},
+			des : ["Do things and be honest.",""]
+		},{
+			banner : ['../images/xingzuo/jx/banner.png'],
+			showList : ['../images/xingzuo/jx/show1.png','../images/xingzuo/jx/show2.png','../images/xingzuo/jx/show3.png','../images/xingzuo/jx/show4.png'],
+			big : ['../images/xingzuo/jx/bannerBig.png'],
+			name : '星座锁骨链-巨蟹座',
+			number : 'Cancer',
+			numberName : '巨蟹座',
+			price : 7399,
+			size : {
+				main : '',
+				color : 'vividgreen',
+				clear : 'vs',
+				cut : '10-20分大小相近的祖母绿',
+				match : ''
+			},
+			des : ["家永远是我们停靠的港湾。",""]
+		},{
+			banner : ['../images/xingzuo/mj/banner.png'],
+			showList : ['../images/xingzuo/mj/show1.png','../images/xingzuo/mj/show2.png','../images/xingzuo/mj/show3.png','../images/xingzuo/mj/show4.png'],
+			big : ['../images/xingzuo/mj/bannerBig.png'],
+			name : '星座锁骨链-摩羯座',
+			number : 'Capricorn',
+			numberName : '摩羯座',
+			price : 7999,
+			size : {
+				main : '',
+				color : 'vividgreen',
+				clear : 'vs',
+				cut : '10-20分大小相近的祖母绿',
+				match : ''
+			},
+			des : ["Always maintain a sense of responsibility and commitment.",""]
+		},{
+			banner : ['../images/xingzuo/ss/banner.png'],
+			showList : ['../images/xingzuo/ss/show1.png','../images/xingzuo/ss/show2.png','../images/xingzuo/ss/show3.png','../images/xingzuo/ss/show4.png'],
+			big : ['../images/xingzuo/ss/bannerBig.png'],
+			name : '星座锁骨链-射手座',
+			number : 'Sagittarius',
+			numberName : '射手座',
+			price : 8499,
+			size : {
+				main : '',
+				color : 'vividgreen',
+				clear : 'vs',
+				cut : '10-20分大小相近的祖母绿',
+				match : ''
+			},
+			des : ["Know how to change, give yourself a way out.",""]
+		},{
+			banner : ['../images/xingzuo/sz/banner.png'],
+			showList : ['../images/xingzuo/sz/show1.png','../images/xingzuo/sz/show2.png','../images/xingzuo/sz/show3.png','../images/xingzuo/sz/show4.png'],
+			big : ['../images/xingzuo/sz/bannerBig.png'],
+			name : '星座锁骨链-狮子座',
+			number : 'Leo',
+			numberName : '狮子座',
+			price : 7799,
+			size : {
+				main : '',
+				color : 'vividgreen',
+				clear : 'vs',
+				cut : '10-20分大小相近的祖母绿',
+				match : ''
+			},
+			des : ["Abandon worry, happiness is the most important.",""]
+		},{
+			banner : ['../images/xingzuo/sy/banner.png'],
+			showList : ['../images/xingzuo/sy/show1.png','../images/xingzuo/sy/show2.png','../images/xingzuo/sy/show3.png','../images/xingzuo/sy/show4.png','../images/xingzuo/sy/show5.png'],
+			big : ['../images/xingzuo/sy/bannerBig.png'],
+			name : '星座锁骨链-双鱼座',
+			number : 'Pisces',
+			numberName : '双鱼座',
+			price : 7599,
+			size : {
+				main : '',
+				color : 'vividgreen',
+				clear : 'vs',
+				cut : '10-20分大小相近的祖母绿',
+				match : ''
+			},
+			des : ["He who gains gains loses",""]
+		},{
+			banner : ['../images/xingzuo/szi/banner.png'],
+			showList : ['../images/xingzuo/szi/show1.png','../images/xingzuo/szi/show2.png','../images/xingzuo/szi/show3.png','../images/xingzuo/szi/show4.png','../images/xingzuo/szi/show5.png'],
+			big : ['../images/xingzuo/szi/bannerBig.png'],
+			name : '星座锁骨链-双子座',
+			number : 'Gemini',
+			numberName : '双子座',
+			price : 7799,
+			size : {
+				main : '',
+				color : 'vividgreen',
+				clear : 'vs',
+				cut : '10-20分大小相近的祖母绿',
+				match : ''
+			},
+			des : ["The past is over, the future is grasped",""]
+		},{
+			banner : ['../images/xingzuo/sp/banner.png'],
+			showList : ['../images/xingzuo/sp/show1.png','../images/xingzuo/sp/show2.png','../images/xingzuo/sp/show3.png','../images/xingzuo/sp/show4.png','../images/xingzuo/sp/show5.png'],
+			big : ['../images/xingzuo/sp/bannerBig.png'],
+			name : '星座锁骨链-水瓶座',
+			number : 'Aquarius',
+			numberName : '水瓶座',
+			price : 6999,
+			size : {
+				main : '',
+				color : 'vividgreen',
+				clear : 'vs',
+				cut : '10-20分大小相近的祖母绿',
+				match : ''
+			},
+			des : ["What you give life is what life gives you",""]
+		},{
+			banner : ['../images/xingzuo/tc/banner.png'],
+			showList : ['../images/xingzuo/tc/show1.png','../images/xingzuo/tc/show2.png','../images/xingzuo/tc/show3.png','../images/xingzuo/tc/show4.png','../images/xingzuo/tc/show5.png'],
+			big : ['../images/xingzuo/tc/bannerBig.png'],
+			name : '星座锁骨链-天秤座',
+			number : 'Libra',
+			numberName : '天秤座',
+			price : 7399,
+			size : {
+				main : '',
+				color : 'vividgreen',
+				clear : 'vs',
+				cut : '10-20分大小相近的祖母绿',
+				match : ''
+			},
+			des : ["Treat anyone and things fairly.",""]
+		},{
+			banner : ['../images/xingzuo/tx/banner.png'],
+			showList : ['../images/xingzuo/tx/show1.png','../images/xingzuo/tx/show2.png','../images/xingzuo/tx/show3.png','../images/xingzuo/tx/show4.png'],
+			big : ['../images/xingzuo/tx/bannerBig.png'],
+			name : '星座锁骨链-天蝎座',
+			number : 'Scorpio',
+			numberName : '天蝎座',
+			price : 8999,
+			size : {
+				main : '',
+				color : 'vividgreen',
+				clear : 'vs',
+				cut : '10-20分大小相近的祖母绿',
+				match : ''
+			},
+			des : ["Treat anyone and things fairly.",""]
+		}]
+	}
+}
+
+function resolveURL(){
+	var search = location.search;
+	search = search.substring(1,search.length);
+	var obj = {};
+	if(search.indexOf('&')>0){
+		var searchArr = search.split('&');
+		obj[searchArr[0].split('=')[0]] = searchArr[0].split('=')[1];
+		obj[searchArr[1].split('=')[0]] = searchArr[1].split('=')[1];
+	}else{
+		obj[search.split('=')[0]] = search.split('=')[1];
+	}
+	return obj
+}
